2f0a2b26027d3a2ad528f47fd72a930dcde1a719
Jay Elbourne <jayelbourne@gmail.com>
2020-03-02 02:03:03 -0500

    Update object_renamer_script.py

--- object_renamer_script.py
+++ object_renamer_script.py
@@ -1,15 +1,33 @@
 from maya import cmds
 
-def rename_objects():
-    selection = cmds.ls(sl=True)
+SUFFIXES = {
+    "mesh": "geo",
+    "joint": "jnt",
+    "ambientLight": "lgt",
+    "camera": None,
+}
 
-    if not selection:
-        selection = cmds.ls(dag=True, long=True)
+DEFAULT_SUFFIX = "grp"
+
+def rename_objects(selection=False):
+    """
+    This function will rename any object to have the correct suffix
+    
+    Args:
+        selection: Whether or not we use the current selection
+    
+    Returns:
+        A list fo all the objects we operated on
+    """
+    objects = cmds.ls(sl=selection, dag=True, long=True)
+
+    # This function cannot run if there is no selection and no objects
+    if selection and not objects:
+        raise RuntimeError("You dont have anything selected.")
         
+    objects.sort(key=len, reverse=True)
 
-    selection.sort(key=len, reverse=True)
-
-    for obj in selection:
+    for obj in objects:
         short_name = obj.split("|")[-1]
         
         children = cmds.listRelatives(obj, children=True, fullPath=True) or []
@@ -19,17 +37,22 @@ def rename_objects():
             obj_type = cmds.objectType(child)
         else:
             obj_type = cmds.objectType(obj)
-        print obj_type    
-        if obj_type == "mesh":
-            suffix = "geo"
-        elif obj_type == "joint":
-            suffix = "jnt"
-        elif obj_type == "camera":
-            print "Skipping camera"
-            continue
-        else:
-            suffix = "grp"
         
-        new_name = short_name + "_" + suffix 
+           
+        suffix = SUFFIXES.get(obj_type, DEFAULT_SUFFIX)
+        
+        if not suffix:
+            continue
+
+        if obj.endswith('_'+suffix):
+            continue
+        
+        new_name = "{0}_{1}".format(short_name, suffix)
         
         cmds.rename(obj, new_name)
+        
+        index = objects.index(obj)
+        
+        objects[index] = obj.replace(short_name, new_name)
+
+    return objects
